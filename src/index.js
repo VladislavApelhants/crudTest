d3b05298388e34077030348f7e832da7f9c5a4c6
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-03-07 18:07:06 +0200

    ds

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/c__post';
+import './js/d__delete';
+import './js/r__get';
+import './js/u__putch';
+// const axios = require('axios/dist/node/axios.cjs');
+import axios from "axios";
+console.log(axios);
